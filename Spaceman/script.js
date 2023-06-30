e4f09394f8ebdd5d75a9b29b97f7547140eac79f
Zakuman8@aol.com <zakuman@DESKTOP-MIFU7C1>
2023-06-30 15:28:56 -0500

    Add event listener to restart button

---
+++ Spaceman/script.js
@@ -0,0 +1,29 @@
+/*----- constants -----*/
+//ABGINZ 
+const correct = 
+  [a,b,g,i,n,z]
+const incorrect = 
+  [c,d,e,f,h,j,k,l,m,o,p,q,r,s,t,u,v,w,x,y]
+
+
+/*----- app's state (variables) -----*/
+
+
+
+
+/*----- cached element references -----*/
+
+
+
+
+/*----- event listeners -----*/
+document.querySelector('main')
+.addEventListener('click', handleChoice);
+
+
+
+/*----- functions -----*/
+init();
+
+
+
